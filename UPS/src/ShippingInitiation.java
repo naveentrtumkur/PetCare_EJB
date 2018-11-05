0d76ee95940d32de786554888960cdf8a376a9c6
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-11-05 17:42:39 -0500

    updated code
    PetCare JEE Rest APIs

--- UPS/src/ShippingInitiation.java
+++ UPS/src/ShippingInitiation.java
@@ -1,10 +1,12 @@
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/initiation")
 public class ShippingInitiation {
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
+	@javax.ws.rs.Produces(MediaType.TEXT_PLAIN)
 	public Response ping() {
 		return Response.ok("Hello, UPS is up and running to serve you ...").build();
 	}
